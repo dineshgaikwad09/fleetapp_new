def411237817d2ce3867b384c55791fa2784f05b
Dinesh Amarsing Gaikwad <dgaikwad@dminc.com>
2020-05-24 01:07:37 +0530

    modified

--- controllers/vehicledetails.controller.js
+++ controllers/vehicledetails.controller.js
@@ -5,10 +5,20 @@ const AWS = require('aws-sdk');
 const multer = require('multer'); // file storing middleware
 const querystring = require('querystring');  
 
-const s3 = new AWS.S3({
-  accessKeyId: 'AKIAJDXJAKI4QK7XMHWA',
-  secretAccessKey: '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'
-});
+// import individual service
+//var S3 = require('aws-sdk/clients/s3');
+
+//var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+//AWS.config.credentials = credentials;
+AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+      console.log("Access key:", AWS.config.credentials.accessKeyId);
+      console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
+    }
+  });
+const s3 = new AWS.S3();
 var sess;
 
 var storage = multer.diskStorage({ 
@@ -127,14 +137,14 @@ exports.insert_vehicle_details = function (req, res) {
                                         fs.readFile(fileName1, (err, data) => {
                                             if (err) throw err;
                                             const params = {
-                                                Bucket: 'fleet-vehicles', // pass your bucket name
+                                                Bucket: 'fleet-photos', // pass your bucket name
                                                 Key: file, // file will be saved as testBucket/contacts.csv
                                                 Body: JSON.stringify(data, null, 2)
                                             };
                                             s3.upload(params, function(s3Err, data) {
                                                 if (s3Err) throw s3Err
                                                 console.log(`File uploaded successfully at ${data.Location}`)
-                                                awsFilePath += data.Location+',';
+                                                awsFilePath += file+',';
                                                 itemsProcessed++;
             
                                                 if(itemsProcessed === files.length) {
@@ -374,25 +384,42 @@ exports.insert_vehicle_details = function (req, res) {
                                     files.forEach(function (file, index) {
                                          const fileName1 = folder + path.join(file);
                                       // Make one pass and make the file complete
-                                        fs.readFile(fileName1, (err, data) => {
-                                            if (err) throw err;
+                                        // fs.readFile(fileName1, (err, data) => {
+                                        //     if (err) throw err;
+                                        //     const params = {
+                                        //         Bucket: 'fleet-photos', // pass your bucket name
+                                        //         Key: file, // file will be saved as testBucket/contacts.csv
+                                        //         Body: JSON.stringify(data, null, 2)
+                                        //     };
+                                        //     s3.upload(params, function(s3Err, data) {
+                                        //         if (s3Err) throw s3Err
+                                        //         console.log(`File uploaded successfully at ${data.Location}`)
+                                        //         awsFilePath += data.Location+',';
+                                        //         itemsProcessed++;
+            
+                                        //         if(itemsProcessed === files.length) {
+                                        //             callback(req, res, awsFilePath);
+                                        //           }
+                                        //     });
+                                        // });
+
+                                         // Read content from the file
+                                        const fileContent = fs.readFileSync(fileName1);
                                             const params = {
-                                                Bucket: 'fleet-vehicles', // pass your bucket name
+                                                Bucket: 'fleet-photos', // pass your bucket name
                                                 Key: file, // file will be saved as testBucket/contacts.csv
-                                                Body: JSON.stringify(data, null, 2)
+                                                Body: fileContent
                                             };
-                                            s3.upload(params, function(s3Err, data) {
+                                             s3.upload(params, function(s3Err, data) {
                                                 if (s3Err) throw s3Err
                                                 console.log(`File uploaded successfully at ${data.Location}`)
-                                                awsFilePath += data.Location+',';
+                                                awsFilePath += file+',';
                                                 itemsProcessed++;
             
                                                 if(itemsProcessed === files.length) {
                                                     callback(req, res, awsFilePath);
                                                   }
                                             });
-                                        });
-                                        
                                     })
                                     function callback (req, res, awsFilePath) { 
                                         console.log('all done'); 
@@ -480,8 +507,19 @@ exports.vehicle_details_view = function (req, res) {
     res.render('vehicleDetails', {session:sess});
 };
 
+
 exports.login_view = function (req, res) {
-    console.log(path);
+    getImage('6AFFFI20707013K.jpg')
+ .then((img)=>{
+  let image="<img src='data:image/jpeg;base64," + encode(img.Body) + "'" + "/>";
+  let startHTML="<html><body></body>";
+  let endHTML="</body></html>";
+  let html=startHTML + image + endHTML;
+  res.send(html)
+  }).catch((e)=>{
+        res.send(e)
+  })
+
     res.sendFile(path.join(path.normalize(__dirname +'/..')+'/public/login.html'));
 };
 
@@ -517,8 +555,68 @@ exports.vehicleInfo = function (req, res) {
                 message: err.message || "Some error occurred while retrieving vehicles."
             });
         }
-        res.render('vehicleInfo', {session:sess, Vehicle:vehicle});
-    })
-    //
-    
+
+        var itemsProcessed = 0;
+        let vehiclesImageData =[];
+        vehicle[0].filename.split(',').forEach(function (key, index) {
+            getImage(key)
+            .then((img)=>{
+             vehiclesImageData.push(encode(img.Body));
+             itemsProcessed++;
+                if(itemsProcessed === vehicle[0].filename.split(',').length-1) {
+                    res.render('vehicleInfo', {session:sess, Vehicle:vehicle, VehiclesImageData: vehiclesImageData});
+                }
+             }).catch((e)=>{
+                console.error(e.message);
+            })
+            //res.render('vehicleInfo', {session:sess, Vehicle:vehicle, VehiclesImageData: vehiclesImageData});
+        });
+    })  
 };
+
+function getS3Object (req, res)
+{
+    var params = {
+        Bucket: "fleet-photos", 
+        Key: "6AFFFI20707013K.jpg"
+       };
+
+       s3.getSignedUrl('getObject', params, function (err, url) {
+        console.log('The URL is', url);
+      });
+       s3.getObject(params, function(err, data) {
+         if (err) console.log(err, err.stack); // an error occurred
+         else     console.log(data);           // successful response
+         /*
+         data = {
+          AcceptRanges: "bytes", 
+          ContentLength: 3191, 
+          ContentType: "image/jpeg", 
+          ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
+          LastModified: <Date Representation>, 
+          Metadata: {
+          }, 
+          TagCount: 2, 
+          VersionId: "null"
+         }
+         */
+       });
+};
+
+function encode(data){
+    let buf = Buffer.from(data);
+    let base64 = buf.toString('base64');
+    console.log(typeof(base64));
+    return base64
+    };
+
+    async function getImage(key){
+        const data =  s3.getObject(
+          {
+              Bucket: 'fleet-photos', 
+              Key: key
+            }
+          
+        ).promise();
+        return data;
+      }
